f435cb35a87ef2fbee4998149fb8203369cb9610
Moise-code <moisewowe@gmail.com>
2023-01-12 02:48:45 -0800

    setting the environment for the scripts

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,15 @@
+// targeting the bars.
+
+const monsterHealthBar = document.querySelector('#monster-health');
+const playerHealthBar = document.querySelector('#player-health');
+const bonusLifeEl = document.querySelector('#bonus-life');
+
+// targeting the buttons to be used in the app.js
+
+const attackBtn = document.querySelector('#attack-btn')
+const strongAttackBtn = document.querySelector('#strong-attack-btn')
+const healBtn = document.querySelector('#heal-btn')
+const logBtn = document.querySelector('#log-btn')
+
+// function to adjust the healthbars.
+
