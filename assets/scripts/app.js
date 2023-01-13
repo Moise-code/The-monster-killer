8e4953a60fbde1b535896098d2fb8eb964fca4f5
Moise-code <moisewowe@gmail.com>
2023-01-13 02:43:21 -0800

    this is the new change to the code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -11,7 +12,15 @@ function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
   monsterPercentage -= ATTACK_VALUE;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage; 
+  if(currentMonsterHealth <= 0 && currentPlayerHealth ){
+    alert('you won the Game cop')
+  } else if(currentPlayerHealth <= 0){
+    alert('the monster killed you. sorry you lost the game')
+  }
   monsterPercentReduce(monsterPercentage); 
+  monsterPosition = monsterPerntReduce('damage * monsterAttack * 4')
 }
 
 attackBtn.addEventListener('click', attackHandler);
