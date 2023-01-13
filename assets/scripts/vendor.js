786ee03fd932ce89becc5b3ea3ee9f7c7bf906eb
Moise-code <moisewowe@gmail.com>
2023-01-13 01:15:44 -0800

    adding the starting js codes

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,28 +1,50 @@
-// targeting the bars.
-
-const monsterHealthBar = document.querySelector('#monster-health');
-const playerHealthBar = document.querySelector('#player-health');
-const bonusLifeEl = document.querySelector('#bonus-life');
-
-// targeting the buttons to be used in the app.js
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+const MonsterPercent = document.getElementById('monsterP')
 
 const attackBtn = document.querySelector('#attack-btn')
 const strongAttackBtn = document.querySelector('#strong-attack-btn')
 const healBtn = document.querySelector('#heal-btn')
 const logBtn = document.querySelector('#log-btn')
 
-// function to adjust the healthbars.
-const adjustHealthBars = (maxLife) =>{
+function adjustHealthBars(maxLife){
   monsterHealthBar.max = maxLife;
   monsterHealthBar.value = maxLife;
   playerHealthBar.max = maxLife
   playerHealthBar.value = maxLife;
+
 }
 
-// function to deal monster damage
+function dealMonsterDamage(damage){
+  const dealtDamage = Math.random() * damage;
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
 
-const dealmonsterDamage = (damage) =>{
-  const dealDamage = Math.random() * damage;
-  monsterHealthBar.value = +monsterHealthBar.value - dealDamage;
-  return dealDamage;
+function monsterPercentReduce(percent){
+  MonsterPercent.textContent = percent;
+}
+
+function dealPlayerDamage(damage) {
+  const dealtDamage = Math.random() * damage;
+  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
+  return dealtDamage;
+}
+
+function increasePlayerHealth(healValue) {
+  playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+  playerHealthBar.value = value;
+  monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
 }
