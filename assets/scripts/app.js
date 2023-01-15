bef072ccd479162194c4343bee0bbfd3b1229b7a
Moise-code <moisewowe@gmail.com>
2023-01-15 01:31:00 -0800

    adding the heal bonus life to the player

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,67 +1,80 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 20;
-const HEAL_VALUE = 22 ;
+const HEAL_VALUE = 22;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let monsterPercentage = 100;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 // attack ,omster function that we are going to call everywhere.
 
-function endRound(){
+function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage; 
-  if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+  currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+
+hasBonusLife = false;
+removeBonusLife();
+currentMonsterHealth = initialPlayerHealth;
+setPlayerHealth(currentPlayerHealth);
+alert('you would be dead but your bonus life recovered you.');
+
+  }
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+
     alert('you won the Game cop')
-  } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('the monster killed you. sorry you lost the game')
-  } else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('You both have a draw.')
   }
 
 }
 
-function attackMonster(mode){
+function attackMonster(mode) {
   let maxDamage;
-  if(mode === 'ATTACK'){
+  if (mode === 'ATTACK') {
     maxDamage = ATTACK_VALUE;
-  }else if(mode === 'STRONG_ATTACK'){
+  } else if (mode === 'STRONG_ATTACK') {
     maxDamage = STRONG_ATTACK_VALUE;
   }
 
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   // monsterPercentage -= ATTACK_VALUE; 
-endRound();  
+  endRound();
 
 }
 function attackHandler() {
   attackMonster('ATTACK')
   // monsterPercentReduce(monsterPercentage - ATTACK_VALUE); 
-   
+
 }
-function strongAttackHandler(){
- 
+function strongAttackHandler() {
+
   attackMonster('STRONG_ATTACK')
 }
 
-function healPlayerHandler(){
-let healValuwing; 
-if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE){
-  alert("you can't heal to more than your max initial health.")
-  healValuwing = chosenMaxLife - currentPlayerHealth;
-} else{
-  healValuwing = HEAL_VALUE;
-}
-increasePlayerHealth(HEAL_VALUE);
-currentPlayerHealth += HEAL_VALUE;
-endRound();
+function healPlayerHandler() {
+  let healValuwing;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("you can't heal to more than your max initial health.")
+    healValuwing = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValuwing = HEAL_VALUE;
+  }
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound();
 }
 
-strongAttackBtn.addEventListener('click', strongAttackHandler )
+strongAttackBtn.addEventListener('click', strongAttackHandler)
 attackBtn.addEventListener('click', attackHandler);
 healBtn.addEventListener('click', healPlayerHandler);
