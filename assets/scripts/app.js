be8ef2d0fb0362aa08b35f63ac0e898fdf3002e3
Moise-code <moisewowe@gmail.com>
2023-01-15 02:48:17 -0800

    adding the log for the player attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,10 +6,20 @@ const HEAL_VALUE = 22;
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
 
+// log events
+
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK'
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK'
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK'
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL'
+const LOG_EVENT_GAME_OVER = 'GAME_OVER'
+
+let battleLog = [];
 // promt show a dialog allow user to enter the value
 
 const enteredValuee = prompt('Maximum life for you and the monster', '100')
 let chosenMaxLife = parseInt(enteredValuee);
+
 
 if(isNaN(chosenMaxLife || chosenMaxLife <=0)){
   chosenMaxLife = 100;
@@ -21,6 +31,32 @@ let monsterPercentage = 100;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(ev, val,targ,monsterHealth, playerHealth){
+
+let logEntry;
+if(ev === LOG_EVENT_PLAYER_ATTACK){
+  logEntry = {
+    event : ev,
+    value : val,
+    target : 'MONSTER',
+    finalMonsterHealth : monsterHealth,
+    finalPlayerHealth : playerHealth
+  };
+  battleLog.push(logEntry);
+
+} else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+  logEntry = {
+    event : ev,
+    value : val,
+    target : 'MONSTER',
+    finalMonsterHealth : monsterHealth,
+    finalPlayerHealth : playerHealth
+  }
+  }
+}
+
+}
 
 function reset(){
  currentMonsterHealth = chosenMaxLife;
